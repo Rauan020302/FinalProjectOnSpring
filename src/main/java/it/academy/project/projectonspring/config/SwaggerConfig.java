6dcb0d7a0a4e5b0e1ab36040c1cb0e430e4309cb
Рауан <ibragimov020302@gmail.com>
2021-06-18 18:05:07 +0600

    Swagger test

--- src/main/java/it/academy/project/projectonspring/config/SwaggerConfig.java
+++ src/main/java/it/academy/project/projectonspring/config/SwaggerConfig.java
@@ -2,23 +2,46 @@ package it.academy.project.projectonspring.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+public class SwaggerConfig implements WebMvcConfigurer {
     @Bean
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(getApiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("it.academy.project.projectonspring.config"))
                 .paths(PathSelectors.any())
                 .build();
 
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry
+                .addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+    private ApiInfo getApiInfo(){
+        return new ApiInfoBuilder()
+                .title("Swagger Api Doc")
+                .description("More Description about the Api")
+                .version("1.0.0")
+                .build();
     }
 }
 
